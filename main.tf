3b6b420f266bca5513fc6ffed8b50afa7be5ce02
Mariia Shytse <aleatoricmbnt@gmail.com>
2024-02-27 16:05:18 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,6 @@
-resource "" "name"{
-  
+resource "null_resource" {
+  count = 7
+  triggers = {
+    always_run = timestamp()
+  }
 }
-
-resource "" "name"{
-  
-}
-
-resource "random_pet" "smth" {}
