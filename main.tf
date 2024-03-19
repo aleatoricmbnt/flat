6c10a7fd92bd0f6f79ad0f462b44d0c889612ccc
Mariia Shytse <aleatoricmbnt@gmail.com>
2024-03-19 15:11:13 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,40 +1,6 @@
-terraform {
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "2.3.0"
-    }
+resource "null_resource" "example" {
+  triggers = {
+    time = timestamp()
   }
 }
-
-data "vault_generic_secret" "default_terraform_version" {
-  path = "kv/tfe/repl"
-}
-
-variable "vault_address" {
-  default = "https://vault.infosec.ic1.statefarm:8200"
-}
-
-variable "vault_namespace" {
-  default = "PCAT"
-}
-
-provider "vault" {
-  address = var.vault_address
-  namespace = var.vault_namespace
-  skip_child_token = true
-  token = "dummy"
-}
-
-variable my_count {
-  default = 60
-}
-
-resource "random_string" "random_string" {
-  count = var.my_count
-  length = 4
-  special = false
-  upper = true
-}
-
 
