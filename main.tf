7a75a7152a21e9fb9f05da2c13d20bd3bfab4861
Mariia Mohylnytska <aleatoricmbnt@gmail.com>
2025-03-31 15:51:08 +0200

    cleared the repo

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 resource "terraform_data" "list_untyped_nested_object" {
-  triggers_replace  = var.list_untyped
+  triggers_replace  = [ var.list_untyped, var.tag_name ]
 }
 
 variable "list_untyped" {
   default = "string"
+}
+
+variable "tag_name" {
+  default = "initial"
 }
--- scalr-policy.hcl
+++
@@ -1,6 +0,0 @@
-version = "v1"
-
-policy "always_failed_soft" {
-  enabled           = true
-  enforcement_level = "soft-mandatory"
-}
