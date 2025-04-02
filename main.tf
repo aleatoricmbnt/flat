e401a6c7f202d7abe86a3785f938f09ca7295608
Mariia Shytse <aleatoricmbnt@gmail.com>
2025-04-02 17:07:47 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,22 @@
-resource "terraform_data" "multiple_triggers" {
-  triggers_replace  = [ var.list_of_tags, var.nested_object ]
-}
+# resource "terraform_data" "multiple_triggers" {
+#   triggers_replace  = [ var.list_of_tags, var.nested_object ]
+# }
 
-variable "list_of_tags" {
-  default = {
-    "tag.with.dot" = "s:://"
-    "slash/suffix" = "f"
-  }
-}
+# variable "list_of_tags" {
+#   default = {
+#     "tag.with.dot" = "s:://"
+#     "slash/suffix" = "f"
+#   }
+# }
 
-variable "nested_object" {
-  default = {
-    upper_level = {
-      "smth.w.dot.and.hash#" = 123
-    }
-  }
+# variable "nested_object" {
+#   default = {
+#     upper_level = {
+#       "smth.w.dot.and.hash#" = 123
+#     }
+#   }
+# }
+
+resource "null_resource" "name" {
+  
 }
