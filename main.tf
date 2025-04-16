da3142026e22956490ca09b1cc3e2bc95bee1db1
aleatoricmbnt <aleatoricmbnt@gmail.com>
2025-04-16 14:41:25 +0300

    test bugs

--- main.tf
+++ main.tf
@@ -1,24 +1,23 @@
-resource "terraform_data" "multiple_triggers" {
-  triggers_replace  = [ var.list_of_tags, var.nested_object ]
-}
+# resource "terraform_data" "multiple_triggers" {
+#   triggers_replace  = [ var.list_of_tags, var.nested_object ]
+# }
 
-variable "list_of_tags" {
-  default = {
-    "tag.with.dot" = "s:://"
-    "slash/suffix" = "f"
-  }
-}
+# variable "list_of_tags" {
+#   default = {
+#     "tag.with.dot" = "s:://"
+#     "slash/suffix" = "f"
+#   }
+# }
 
-variable "nested_object" {
-  default = {
-    upper_level = {
-      "smth.w.dot.and.hash#" = 123
-    }
-  }
-}
+# variable "nested_object" {
+#   default = {
+#     upper_level = {
+#       "smth.w.dot.and.hash#" = 123
+#     }
+#   }
+# }
 
-resource "terraform_data" "replacement" {
-  input = var.input
+resource "scalr_workspace" "cli-driven" {
+  name           = "created_automatically"
+  environment_id = "env-v0ns0m539r05815rg"
 }
-
-variable "input" {}
